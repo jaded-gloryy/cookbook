53b6547dbf8b9325842ee024b3d0928c0c0f5a71
Jade Burgin <jjr1945@rit.edu>
2022-11-29 11:33:24 -0500

    Define class for creating ingredient objects and measurements

--- classes/ingredient.py
+++ classes/ingredient.py
@@ -1,1 +1,31 @@
 #will have measurement type mass, volume, or size
+from . import measurement
+
+class ingredient:
+    """
+    An ingredient object has several attributes:
+        Name: The name of the ingredient
+        Matter: Solid or liquid
+        Measurement_type: Volume or mass
+        Food_group: Fruit, vegetable, grain, protein, or dairy
+        
+    
+    """
+    def __init__(self, name, matter, measurement_type, food_group):
+        self.name = name
+        self.mass_type = matter
+        self.measurement_type = measurement_type
+        self.food_group = food_group
+
+    def assign_units(self, units):
+        """
+        Used to assign units of measurement (cups, grams, whole, etc)
+        """
+        self.units = units
+    
+    def assign_measurement(self, amount, units):
+        self.amount = amount
+        self.assign_units(units)
+
+
+            
