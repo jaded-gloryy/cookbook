13f7b9de498616b8ad46ff40eb3721b33785e734
Jade Burgin <jjr1945@rit.edu>
2022-12-01 22:04:54 -0500

    add comments: plan functionality

---
+++ src/classes/ServingSize.py
@@ -0,0 +1,5 @@
+# multiplier or divisor 
+# based on measurement types
+#   (cups are bucketed into halves, quarters, eighths (also an integer) maybe a float? bc I want to round)
+#   (volume is usually whole integers)
+#   (mass can be decimal integer)
