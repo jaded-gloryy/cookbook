581ebc4302e01178232dddf2cb15356e06ebbd6c
Jade Burgin <jjr1945@rit.edu>
2022-12-01 22:03:27 -0500

    create planned funcions

---
+++ src/recommender/recommender_functions.py
@@ -0,0 +1,19 @@
+def recommend_by_ingredients():
+    return
+
+def recommend_by_meal_type():
+    return
+
+def recommend_random_recipe():
+    # use a recommendation algorithm to suggest?
+    return
+
+def request_recommendation_feedback():
+    """
+    A function requesting a user to rate a recommendation.
+
+    - Not interested
+    - Maybe later
+    - Not today
+    - Yes please!
+    """
