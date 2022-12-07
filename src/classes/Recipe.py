a0f67b0da56a896b77fdb59d2825e6644393f673
Jade Burgin <jjr1945@rit.edu>
2022-12-07 11:58:31 -0500

    Update nomenclatured for an ingredient+measurement pair

--- src/classes/Recipe.py
+++ src/classes/Recipe.py
@@ -6,14 +6,14 @@ class Recipe:
     """
     A recipe object has several attributes:
         Name: The name of the recipe
-        Ingredients: A list of ingredients dicts {ingredient : measurement}
+        Ingredient section: A list of ingredients dicts {ingredient : measurement}
         Directions: A dict of steps on how to prepare the recipe
 
         TODO: Add a way to assign a pic for a recipe    
     """
-    def __init__(self, name, ingredients, directions):
+    def __init__(self, name, measured_ingredients, directions):
         self.name = name
-        self.ingredients = ingredients
+        self.measured_ingredients = measured_ingredients
         self.directions = directions
 
     # def create_recipe():
@@ -31,13 +31,13 @@ class Component(Recipe):
     """
     Component is a subclass of a Recipe. 
     """
-    def __init__(self, name,ingredients, directions):
+    def __init__(self, name,measured_ingredients, directions):
         self.name = name
-        Recipe.__init__(self, ingredients, directions)
+        Recipe.__init__(self, measured_ingredients, directions)
        
        
-    def assign_ingredient(self, ingredient):   
-        self.ingredients = ingredient
+    def assign_ingredient(self, measured_ingredients):   
+        self.measured_ingredients = measured_ingredients
 
     def assign_directions(self, directions):
         self.directions = directions
