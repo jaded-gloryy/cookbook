c2f2c5090c40a924b79a12491cd63bdde2e18867
Jade Burgin <jjr1945@rit.edu>
2022-12-07 17:36:24 -0500

    Move to flows folder to organize repo and make functions reusable

---
+++ src/flows/connect_to_db.py
@@ -0,0 +1,18 @@
+import certifi
+from pymongo import MongoClient
+from classes.DatabaseClient import NoSQLDatabaseClient
+from config import CONFIG
+
+def connect_to_db(database_name):
+    """
+    Function to connect to cookbook db. 
+    """
+    print("Setting up Database Connection...")
+    mongo_client = MongoClient(CONFIG["CONNECTION_STRING"], serverSelectionTimeoutMS=5000, tlsCAFile=certifi.where())
+    print("Successfully connected to Database")
+
+    print("Creating Database Client...")
+    db_client = NoSQLDatabaseClient(mongo_client, database_name )
+    print("Database Client created")
+    
+    return db_client
