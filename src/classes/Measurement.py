c1a3e440d491f901f76dadec213a94dbdd53d411
Jade Burgin <jjr1945@rit.edu>
2022-12-07 11:57:33 -0500

    Add measurement creation helper functions

--- src/classes/Measurement.py
+++ src/classes/Measurement.py
@@ -1,6 +1,7 @@
 #will have a integer and measurement type (cups, teaspoons, tablespoons --> volume, mass, size?)
-class Measurement():
-    def _init_(self, quantity, unit):
+class Measurement:
+
+    def __init__(self, quantity, unit):
         self.quantity = quantity
         self.unit = unit
         # #matter= liquid or solid
@@ -8,13 +9,34 @@ class Measurement():
         # #measurement type = cups, teaspoons, grams, etc
         # self.measurement_type = measurement_type
 
-    # def create_measurement():
-    #     """
-    #     A function to combine 
-    #     """
-    
     # def get_measurement_type(self, ingredient_name):
     #     """
     #     A function to query the ingredient for the type of measurement (volume or mass)
     #     """
     #     ingredient_name = 
+
+
+    def create_measurement(quantity, unit):
+        """
+        A function to combine a quantity with unit
+        Inputs: quantity: int, unit: str
+        Output: A measurement object
+        """
+        measurement_object = Measurement(quantity, unit)
+        return measurement_object
+
+    def createMany_measurements(measurement_data):
+        """
+        Helper function to generate a list of measurement objects for recipe creation.
+        Input: list of quantity, unit pairs;
+            quantity: int, unit: str
+        Output: A list of measurement objects
+            
+        """
+        measurement_objects = []
+        for measurement in measurement_data:
+            new_measurement = Measurement.create_measurement(measurement[0], measurement[1])
+
+            measurement_objects.append(new_measurement)
+        
+        return measurement_objects
