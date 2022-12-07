6dfd8498b1bc4538ddde207fefec587907251c90
Jade Burgin <jjr1945@rit.edu>
2022-12-07 12:27:09 -0500

    Add processes to connect ta a db, ingest recipe data, create a new recipe object, and upload data.

--- src/app.py
+++ src/app.py
@@ -4,72 +4,15 @@ from pymongo import MongoClient
 from classes.DatabaseClient import NoSQLDatabaseClient
 from classes.Ingredient import Ingredient
 from classes.Recipe import Recipe
+from classes.Measurement import Measurement
 from config import CONFIG
 from recipe_flows import create_recipe
+import ingredient_flows as iflow
 
 # three steps:
 
 #connect to db
-def standardize_ingredient_name(ingredient_name):
-    ingredient = ingredient_name.lower()
-    standardized_ingredient_name = ingredient.replace(" ","_")
-
-    return standardized_ingredient_name
-
-def request_ingredient_data(csv_filepath):
-    """
-    Used to request data for ingredient object creation.
-    Requests user to provide a filepath to a csv containing the following ingredient data:
-
-    Name: str; The name of the ingredient
-    Matter: str; Solid or liquid
-    Food_group: str; Ex: Fruit, vegetable, grain, protein, dairy, spice, activator, sweetener, oil
-    Quantity: int; Quantity of the ingredient needed in a recipe
-    Units: str; The units of measure for the ingredient (ex, cups, tsp)
-
-    Input: filepath
-    Output: 3 lists: ingredient names, 
-    (name, matter, food_group),
-    (quantity, units)
-    """
-    print("Importing recipe data...")
-    data = pd.read_csv(csv_filepath)
-    df = pd.DataFrame(data)
-    df_removena = df.dropna(thresh=3)
-    headings= list(df_removena.columns)
-    ingredient_names = list(df_removena.name)
-
-    standardized_ingredient_names = []
-    
-    #Grab headings from table and designate as keys
-    # keys = []
-    # for heading in headings:
-    #     keys.append(heading.lower())
-
-    #parse ingredient names and standardize format
-    for dirty in ingredient_names:
-        clean = standardize_ingredient_name(dirty)
-        standardized_ingredient_names.append(clean)
-
-    #ingredient data (should be a tuple: ingredient name, matter, food group)
-    ingredient_data = []
-    measurement_data = []
-    for row in range(len(df_removena)):
-        name = df_removena.name.iloc[row]
-        matter = df_removena.matter.iloc[row]
-        foodgroup = df_removena.food_group.iloc[row]
-        quantity = df_removena.quantity.iloc[row]
-        units = df_removena.units.iloc[row].lower()
-
-        ingredient_data.append([name,matter,foodgroup])
-        measurement_data.append([quantity, units])
-
-
-    print("Successfully imported recipe data")
-    return standardized_ingredient_names, ingredient_data, measurement_data
-
-
-def connect_to_db():
+def connect_to_db(database_name):
     """
     Function to connect to cookbook db. 
     """
@@ -78,14 +21,23 @@ def connect_to_db():
     print("Successfully connected to Database")
 
     print("Creating Database Client...")
-    db_client = NoSQLDatabaseClient(mongo_client, CONFIG["DATABASE_NAME"])
+    db_client = NoSQLDatabaseClient(mongo_client, database_name )
     print("Database Client created")
     
     return db_client
 
-ingredient_collection = CONFIG["INGREDIENT_COLLECTION_NAME"]
-recipe_collecition = CONFIG["RECIPE_COLLECTION_NAME"]
+# Comment when testing
+# database_name = CONFIG["DATABASE_NAME"]
+# ingredient_collection = CONFIG["INGREDIENT_COLLECTION_NAME"]
+# recipe_collecition = CONFIG["RECIPE_COLLECTION_NAME"]
 recipe_filepath = CONFIG["RECIPE_FILEPATH"]
+directions_filepath = CONFIG["DIRECTIONS_FILEPATH"]
+recipe_name = "Pizza"
+
+# Uncomment when testing
+database_name = CONFIG["TEST_DATABASE_NAME"]
+ingredient_collection = CONFIG["TEST_COLLECTION_NAME"]
+recipe_collecition = CONFIG["TEST_COLLECTION_NAME"]
 
 #1. Create recipes
 #a. read in ingredients using request_ingredient_data
@@ -102,22 +54,25 @@ recipe_filepath = CONFIG["RECIPE_FILEPATH"]
 
 
 if __name__ == "__main__":
-    standardized_ingredient_names, ingredient_data, measurement_data = request_ingredient_data(recipe_filepath)
-    db_client = connect_to_db() 
-    create_new_ingredients , non_existent_ingredients = db_client.query_ingredient(ingredient_collection, standardized_ingredient_names)
-
-    if create_new_ingredients == True:
-        print("Creating and uploading " + len(create_new_ingredients)) + " ingredient objects..."
-        for ingredient in non_existent_ingredients:
-            new_ingredient = Ingredient(ingredient_data[0], ingredient_data[1], ingredient_data[2])
-            db_client.upload_ingredient(ingredient_collection, new_ingredient)
+    # Read in data for recipe
+    standardized_ingredient_names, ingredient_data, measurement_data = iflow.request_ingredient_data(recipe_filepath)
+    directions = iflow.request_direction_data(directions_filepath)
+    db_client = connect_to_db(database_name) 
     
-
-    #make measure obs
-
+    #Check for ingredients in db
+    create_new_ingredients , non_existent_ingredients = db_client.query_ingredient(ingredient_collection, standardized_ingredient_names)
+    if create_new_ingredients == True:
+        new_ingredients = iflow.create_ingredients(ingredient_data, non_existent_ingredients)
+        db_client.uploadMany_ingredients(ingredient_collection, new_ingredients)
+    
+    # At this point all ingredients exist in the db
+    ingredients = db_client.get_ingredients(ingredient_collection, standardized_ingredient_names)
+    measurements = Measurement.createMany_measurements(measurement_data)
 
     #create Recipe
-    recipe = Recipe()
+    new_recipe = create_recipe(recipe_name,ingredients,measurements,directions)
+    
+
 
 
 
