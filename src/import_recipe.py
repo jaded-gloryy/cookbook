472313c1cde5b4bf678fc35571cd8bd61f035fe6
Jade Burgin <jjr1945@rit.edu>
2022-12-08 16:32:09 -0500

    Add logic to check if recipe already exists

--- src/import_recipe.py
+++ src/import_recipe.py
@@ -2,7 +2,7 @@ from classes.Measurement import Measurement
 from config import CONFIG
 from flows.recipe_flows import create_recipe
 import flows.ingredient_flows as iflow
-from flows.connect_to_db import connect_to_db
+from utils import connect_to_db, standardize_name
 
 recipe_filepath = CONFIG["RECIPE_FILEPATH"]
 directions_filepath = CONFIG["DIRECTIONS_FILEPATH"]
@@ -18,29 +18,38 @@ recipe_name = input("What is the name of this recipe?")
 # database_name = CONFIG["TEST_DATABASE_NAME"]
 # ingredient_collection = CONFIG["TEST_COLLECTION_NAME"]
 # recipe_collection = CONFIG["TEST_COLLECTION_NAME2"]
-# recipe_name = "pizza"
+# recipe_name = "pizZa"
 
 if __name__ == "__main__":
     
-    # Read in data for recipe
-    standardized_ingredient_names, ingredient_data, measurement_data = iflow.request_ingredient_data(recipe_filepath)
-    directions = iflow.request_direction_data(directions_filepath)
-    db_client = connect_to_db(database_name) 
+    db_client = connect_to_db(database_name)
     
-    #Check for ingredients in db
-    create_new_ingredients , non_existent_ingredients = db_client.query_ingredient(ingredient_collection, standardized_ingredient_names)
-    if create_new_ingredients == True:
-        new_ingredients = iflow.create_ingredients(ingredient_data, non_existent_ingredients)
-        db_client.uploadMany_ingredients(ingredient_collection, new_ingredients)
+    # Check if the recipe has already been created
+    recipe_exists = db_client.get_recipe(recipe_collection, standardize_name(recipe_name))
     
-    # At this point all ingredients exist in the db
-    queried_ingredient_data = db_client.get_ingredients(ingredient_collection, standardized_ingredient_names)
-    ingredients = iflow.get_instances(queried_ingredient_data)
-    measurements = Measurement.createMany_measurements(measurement_data)
+    if recipe_exists != None:
+        print("This recipe has already been created. Please use update_recipe.py to make edits.")
+    
+    else:
+        # Read in data for recipe
+        standardized_ingredient_names, ingredient_data, measurement_data = iflow.request_ingredient_data(recipe_filepath)
+        directions = iflow.request_direction_data(directions_filepath)
+        
+        
+        #Check for ingredients in db
+        create_new_ingredients , non_existent_ingredients = db_client.query_ingredient(ingredient_collection, standardized_ingredient_names)
+        if create_new_ingredients == True:
+            new_ingredients = iflow.create_ingredients(ingredient_data, non_existent_ingredients)
+            db_client.uploadMany_ingredients(ingredient_collection, new_ingredients)
+        
+        # At this point all ingredients exist in the db
+        queried_ingredient_data = db_client.get_ingredients(ingredient_collection, standardized_ingredient_names)
+        ingredients = iflow.get_instances(queried_ingredient_data)
+        measurements = Measurement.createMany_measurements(measurement_data)
 
-    #create Recipe
-    new_recipe = create_recipe(recipe_name,ingredients,measurements,directions)
-    db_client.uploadOne_recipe(recipe_collection,new_recipe)
+        #create Recipe
+        new_recipe = create_recipe(recipe_name,ingredients,measurements,directions)
+        db_client.uploadOne_recipe(recipe_collection,new_recipe)
     
 
 
