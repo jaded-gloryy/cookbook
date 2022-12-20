adbd7bcea4ecac86c4937762d7be9383612bbfae
Jade Burgin <jjr1945@rit.edu>
2022-12-20 10:42:57 -0500

    Remove createMany_measurement function because a Measurement has to be initialized before using class methods

--- src/classes/Measurement.py
+++ src/classes/Measurement.py
@@ -14,20 +14,3 @@ class Measurement:
     #     A function to query the ingredient for the type of measurement (volume or mass)
     #     """
     #     ingredient_name = 
-
-
-    def createMany_measurements(measurement_data):
-        """
-        Helper function to generate a list of measurement objects for recipe creation.
-        Input: list of quantity, unit pairs;
-            quantity: int, unit: str
-        Output: A list of measurement objects
-            
-        """
-        measurement_objects = []
-        for measurement in measurement_data:
-            new_measurement = Measurement.create_measurement(measurement[0], measurement[1])
-
-            measurement_objects.append(new_measurement)
-        
-        return measurement_objects
